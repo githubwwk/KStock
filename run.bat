7b54f2fb0ac0593c97e31089800936750b2fba9a
Konrad Wei <konrad.wei@gmail.com>
2017-07-25 23:46:24 +0800

    update
    update

--- run.bat
+++ run.bat
@@ -2,15 +2,17 @@
 @ECHO OFF
 FOR /F "TOKENS=*" %%A IN ('PN_DT.exe DT') DO SET DATE_TIME=%%A
 
-:: Backup MongoDB
 
+
+:: Backup MongoDB
 set PATH=%PATH%;C:\Program Files\MongoDB\Server\3.4\bin
+::mongod --dbpath D:\Work\mongodb\data --logpath=D:\Work\mongodb\log\log.txt --install
 mongodump -h 127.0.0.1:27017 -d kStock -o ./backup/kStock-mongodb
 
 :: Backup MongoDB 
 @cd backup
 @cd kStock-mongodb
-@7z a -r .\kStock_%DATE_TIME% .\kStock
+::@7z a -r .\kStock_%DATE_TIME% .\kStock
 cd ..
 cd ..
 
